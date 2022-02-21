fc2b3e7c08667ce6f4e911cb5804a0475e246eb6
Matias Herranz <matiasherranz@gmail.com>
2022-02-21 12:25:12 -0300

    MapBox POC integration.

---
+++ components/MapboxPOC.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect, useState } from 'react'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+
+import styles from './MapboxPOC.module.scss'
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWF0aWFzaGVycmFueiIsImEiOiJja3drenRhbGoxeDZtMnZuczFyd2lzbDJyIn0.DcL4Jp8fJ1XY2PtKggGxaA'
+
+const MapboxPOC = () => {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
+
+  useEffect(() => {
+    if (!map.current) return // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
+    })
+  })
+
+  return (
+    <div className={styles.mapWrapper}>
+      <div className={styles.sidebar}>
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className={styles.mapContainer} />
+    </div>
+  )
+}
+
+export default MapboxPOC
