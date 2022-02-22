7aa39554711543ae1b104feaad4f2779f046870e
Matias Herranz <matiasherranz@gmail.com>
2022-02-22 10:46:27 -0300

    Added debounce.

--- components/MapboxPOC.tsx
+++ components/MapboxPOC.tsx
@@ -1,19 +1,29 @@
 import React, { useRef, useEffect, useState } from 'react'
+import ReactMapboxGl, { Marker } from '../../../'
 
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
+import useDebounce from './useDebounce'
 import styles from './MapboxPOC.module.scss'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibWF0aWFzaGVycmFueiIsImEiOiJja3drenRhbGoxeDZtMnZuczFyd2lzbDJyIn0.DcL4Jp8fJ1XY2PtKggGxaA'
 
+const API = 'http://0.0.0.0:7680/api/v1/lender_search/map'
+
 const MapboxPOC = () => {
   const mapContainer = useRef(null)
   const map = useRef<mapboxgl.Map | undefined>(undefined)
-  const [lng, setLng] = useState(-70.9001)
-  const [lat, setLat] = useState(42.3501)
-  const [zoom, setZoom] = useState(9)
+  const [lng, setLng] = useState(-70)
+  const [lat, setLat] = useState(42)
+
+  const [dZoom, zoom, setZoom] = useDebounce(9, 500)
+  // TODO: Initialize this to something more meaningful
+  const [dBounds, bounds, setBounds] = useDebounce(
+    { tl: { lat, lng }, br: { lat, lng } },
+    500
+  )
 
   useEffect(() => {
     if (map.current) return // initialize map only once
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import MapboxPOC from '../components/MapboxPOC'
 
